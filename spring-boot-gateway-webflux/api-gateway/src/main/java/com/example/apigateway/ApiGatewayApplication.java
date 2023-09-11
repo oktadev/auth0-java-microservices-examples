bac4c54e86990cad0046501597d9c3fd782a3c38
Matt Raible <matt@raibledesigns.com>
2023-09-11 09:30:27 -0600

    Optimize imports

--- spring-boot-gateway-webflux/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ spring-boot-gateway-webflux/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -4,25 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.stream.StreamSupport;
@@ -40,19 +27,19 @@ public class ApiGatewayApplication {
     // @EventListener
     public void handleContextRefresh(ContextRefreshedEvent event) {
         final Environment env = event.getApplicationContext()
-            .getEnvironment();
+                .getEnvironment();
 
         LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
 
         final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
 
         StreamSupport.stream(sources.spliterator(), false)
-            .filter(ps -> ps instanceof EnumerablePropertySource)
-            .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
-            .flatMap(Arrays::stream)
-            .distinct()
-            .filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
-            .forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
+                .filter(ps -> ps instanceof EnumerablePropertySource)
+                .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
+                .flatMap(Arrays::stream)
+                .distinct()
+                .filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
+                .forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
     }
 }
 
