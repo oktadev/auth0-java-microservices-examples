8a915a513b4d9fb07cd65d0b0daf011087e8ec74
Matt Raible <matt@raibledesigns.com>
2023-12-10 18:36:01 -0700

    Upgrade to Spring Boot 3.2

---
+++ spring-boot-gateway-mvc/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,59 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
+
+record Car(String name) {
+}
+
+@FeignClient("car-service")
+interface CarClient {
+
+    @GetMapping("/cars")
+    CollectionModel<Car> readCars();
+}
+
+@RestController
+class CoolCarController {
+
+    private final CarClient carClient;
+
+    public CoolCarController(CarClient carClient) {
+        this.carClient = carClient;
+    }
+
+    @GetMapping("/cool-cars")
+    public Collection<Car> coolCars() {
+        return carClient.readCars()
+                .getContent()
+                .stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !car.name().equals("AMC Gremlin") &&
+                !car.name().equals("Triumph Stag") &&
+                !car.name().equals("Ford Pinto") &&
+                !car.name().equals("Yugo GV");
+    }
+}
