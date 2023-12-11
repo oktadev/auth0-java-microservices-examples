fa1a3e97f2496da303ce3f036bdc288c2aef4482
Matt Raible <matt@raibledesigns.com>
2023-12-11 12:32:13 -0700

    Add Spring Cloud Circuit Breaker support

--- spring-boot-gateway-mvc/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ spring-boot-gateway-mvc/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.RouterFunction;
@@ -26,17 +27,15 @@ import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequ
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-    //@Bean
+    @Bean
     public RouterFunction<ServerResponse> gatewayRouterFunctionsLoadBalancer() {
-        // TODO: token relay not workinig in config
-        // @formatter:off
         return route("car-service")
-                .route(path("/home/**"), http())
-                .filter(lb("car-service"))
-                .filter(tokenRelay())
-                .build();
-        // @formatter:on
+            .route(path("/home/**"), http())
+            .filter(lb("car-service"))
+            .filter(tokenRelay())
+            .build();
     }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
@@ -45,11 +44,21 @@ public class ApiGatewayApplication {
 record Car(String name) {
 }
 
-@FeignClient("car-service")
+@FeignClient(name = "car-service", fallback = Fallback.class)
 interface CarClient {
 
     @GetMapping("/cars")
     CollectionModel<Car> readCars();
+
+}
+
+@Component
+class Fallback implements CarClient {
+
+    @Override
+    public CollectionModel<Car> readCars() {
+        return CollectionModel.empty();
+    }
 }
 
 @RestController
@@ -64,16 +73,16 @@ class CoolCarController {
     @GetMapping("/cool-cars")
     public Collection<Car> coolCars() {
         return carClient.readCars()
-                .getContent()
-                .stream()
-                .filter(this::isCool)
-                .collect(Collectors.toList());
+            .getContent()
+            .stream()
+            .filter(this::isCool)
+            .collect(Collectors.toList());
     }
 
     private boolean isCool(Car car) {
         return !car.name().equals("AMC Gremlin") &&
-                !car.name().equals("Triumph Stag") &&
-                !car.name().equals("Ford Pinto") &&
-                !car.name().equals("Yugo GV");
+            !car.name().equals("Triumph Stag") &&
+            !car.name().equals("Ford Pinto") &&
+            !car.name().equals("Yugo GV");
     }
 }
