b29b9eb1930cc1339e5f8e4405d3d7e38df0a1a8
Matt Raible <matt@raibledesigns.com>
2023-12-11 20:12:51 -0700

    Add completed MVC example

---
+++ spring-boot-gateway-mvc/car-service/src/main/java/com/example/carservice/web/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.carservice.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    private final static Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/home")
+    public String howdy(Principal principal) {
+        String username = principal.getName();
+        if (principal instanceof JwtAuthenticationToken token) {
+            log.info("claims: " + token.getTokenAttributes());
+        }
+        return "Hello, " + username;
+    }
+}
