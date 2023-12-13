2ff9f6e7074d199168057ed1006e2fa310fb832e
Matt Raible <matt@raibledesigns.com>
2023-12-13 00:08:29 -0700

    I prefer spaces

--- spring-boot-gateway-mvc/api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
+++ spring-boot-gateway-mvc/api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- spring-boot-gateway-mvc/discovery-service/src/test/java/com/example/discoveryservice/EurekaServiceApplicationTests.java
+++ spring-boot-gateway-mvc/discovery-service/src/test/java/com/example/discoveryservice/EurekaServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
